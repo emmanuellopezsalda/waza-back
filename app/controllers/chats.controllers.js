44baa63f43cce1876988eab6646f3ec2df29f259
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-09-21 15:43:58 -0500

    mensajes listos

--- app/controllers/chats.controllers.js
+++ app/controllers/chats.controllers.js
@@ -3,12 +3,21 @@ import { db } from "../config/db.js";
 export const getChats = async(req, res) => {
     const {id_user_1} = req.params;
     try {
-        const request = await db.query("SELECT * FROM chats WHERE id_user_1 = ?", [id_user_1]);
-        res.json(request[0])
+        const request = await db.query(`SELECT c.id AS chat_id, 
+            CASE 
+            WHEN c.id_user_1 = ? THEN u2.name 
+            WHEN c.id_user_2 = ? THEN u1.name 
+            END AS other_user_name
+            FROM chats c
+            INNER JOIN users u1 ON c.id_user_1 = u1.id
+            INNER JOIN users u2 ON c.id_user_2 = u2.id
+            WHERE ? IN (c.id_user_1, c.id_user_2);`, [id_user_1, id_user_1, id_user_1]);
+        res.json(request[0]);
     } catch (error) {
-        res.json({error})
+        res.json({ error });
     }
 }
+
 export const postChat = (req, res) => {
     const {id_user_1, id_user_2} = req.body;
     try {
--- app/controllers/messages.controllers.js
+++ app/controllers/messages.controllers.js
@@ -4,7 +4,7 @@ import { db } from "../config/db.js";
 export const getMessages = async(req, res) => {
     const {id_chat} = req.params;
     try {
-        const request = await db.query("SELECT * FROM messages WHERE id_chat", [id_chat]);
+        const request = await db.query("SELECT * FROM messages WHERE id_chat = ?", [id_chat]);
         res.json(request[0]);
     } catch (error) {
         res.json(error);
