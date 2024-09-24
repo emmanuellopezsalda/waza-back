038d122b05a86879164fcc442ea9be56ad685114
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-09-24 16:16:03 -0500

    sajd

--- app/server.js
+++ app/server.js
@@ -2,33 +2,12 @@ import express from 'express';
 import routes from './routes/index.js';
 import cors from 'cors';
 import createWebSocketServer from './sockets/websocket.js';
-// import { createRequire } from 'module';
-// const require = createRequire(import.meta.url);
-// const WebSocket = require('ws');
 
 const server = express();
-// const wss = new WebSocket.Server({ port: 3200 });
 
 server.use(express.json());
 server.set("port", process.env.PORT || 3000);
 server.use(cors());
-
-// wss.on('connection', (ws) => {
-//     console.log('Nuevo cliente conectado');
-    
-//     ws.on('message', (message) => {
-//         console.log(`Mensaje recibido: ${message}`);        
-//         wss.clients.forEach(client => {
-//             if (client.readyState === WebSocket.OPEN) {
-//                 client.send(message.toString());
-//             }
-//         });
-//     });
-
-//     ws.on('close', () => {
-//         console.log('Cliente desconectado');
-//     });
-// });
 server.use("/", routes);
 createWebSocketServer();
 export default server;
